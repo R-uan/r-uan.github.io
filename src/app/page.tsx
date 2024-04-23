5604f9b5a385b969671bcad1c645201dc3213ec2
Ruan <ruan.oliveiralopes@hotmail.com>
2024-04-23 08:48:05 -0300

    extra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Navigation from "./(Components)/(Navigation)/Navigation";
 import Main from "./(Landing)/Landing";
-import Repositories from "./(Repositories)/Repositories";
+import Repositories from "./(Repositories)/About";
 
 export default function Home() {
 	return (
-		<body className="outline-[1px]  p-[15px] outline outline-[#ff003c] overflow-auto min-h-screen outline-offset-[-10px] bg-[#0b0e14]">
+		<body className="outline-[1px] p-[15px] outline outline-[#ff003c] overflow-auto min-h-screen outline-offset-[-10px] bg-[#0b0e14]">
 			<Navigation />
 			<Main />
 			<Repositories />
