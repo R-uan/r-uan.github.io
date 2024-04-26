72a2ae7ba4eb464d3d9f304d85b80a8abc710ca7
Ruan <ruan.oliveiralopes@hotmail.com>
2024-04-26 04:20:15 -0300

    Create Socials.ts

---
+++ src/app/Socials.ts
@@ -0,0 +1,5 @@
+export const socials = {
+	github: "https://github.com/R-uan",
+	linkedin: "https://www.linkedin.com/in/ruan-lopes-12041b1bb/",
+	steam: "https://steamcommunity.com/id/notactuallyaweeb/",
+};
