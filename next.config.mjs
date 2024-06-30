52fdf429c1d94da027a0e669b2ee7fc00d56fc51
Ruan <75811206+R-uan@users.noreply.github.com>
2024-06-30 04:18:42 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	/* 	basePath: "/",
+	basePath: "/",
 	output: "export",
 	reactStrictMode: true,
- */
 };
 
 export default nextConfig;
